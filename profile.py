f850c6df869d62f97fb84e3e6fc4a70ea643053e
Keksi <contact@keksi.me>
2024-02-17 11:31:53 +0100

    Add profile.py

---
+++ profile.py
@@ -0,0 +1,8 @@
+from github import Profile
+
+class Bio(Profile):
+    name    = "Keksi♡"
+    age     = "19"
+    country = "Germany"
+    email   = "contact@keksi.me"
+    website = "https://keksi.me"
