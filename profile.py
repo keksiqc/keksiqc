d882666f17c31702ead589e9f43b5cd0f4db9d38
Keksi <contact@keksi.me>
2024-12-08 11:04:50 +0100

    chore(profile.py): update age from 19 to 20

--- profile.py
+++ profile.py
@@ -2,7 +2,7 @@ from github import Profile
 
 class Bio(Profile):
     name    = "Keksi"
-    age     = "19"
+    age     = "20"
     country = "Germany"
     email   = "contact@keksi.me"
     website = "https://keksi.dev"
