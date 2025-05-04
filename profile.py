996100fca58b07d33295b588c18d4c946c1cbe6d
Keksi <me@keksi.dev>
2025-05-04 21:14:57 +0200

    Update profile.py

--- profile.py
+++ profile.py
@@ -2,7 +2,7 @@ from github import Profile
 
 class Bio(Profile):
     name    = "Keksi"
-    age     = "20"  # Correct age
+    age     = "20"
     country = "Germany"
     email   = "hello@keksi.dev"
     website = "https://keksi.dev"
